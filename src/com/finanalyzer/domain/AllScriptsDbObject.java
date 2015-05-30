477dcf8bb57b7bae75438e56d21ac038668e4e01
sreenathchakkingal <sreenathchakkingal04@gmail.com>
2015-05-30 19:23:43 +0530

    selector

--- src/com/finanalyzer/domain/AllScriptsDbObject.java
+++ src/com/finanalyzer/domain/AllScriptsDbObject.java
@@ -6,10 +6,12 @@ import java.util.List;
 import com.finanalyzer.db.AllScripsUtil;
 import com.finanalyzer.db.DbOperations;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.gs.collections.api.block.function.Function;
 import com.gs.collections.impl.list.mutable.FastList;
 
 public class AllScriptsDbObject extends DbObject{
@@ -28,6 +30,23 @@ public class AllScriptsDbObject extends DbObject{
 	private String fairValue;
 	private String industry;
 
+	public final Function<Entity, AllScriptsDbObject> DB_OBJECT_SELECTOR = new Function<Entity, AllScriptsDbObject>()
+	{
+
+		@Override
+		public AllScriptsDbObject valueOf(Entity entity)
+		{
+			return new AllScriptsDbObject(
+					(String) entity.getProperty(NSE_ID),
+					(String) entity.getProperty(STOCK_NAME),
+					(String) entity.getProperty(ISIN),
+					(String) entity.getProperty(BSE_ID),
+					(String) entity.getProperty(FAIR_VALUE),
+					(String) entity.getProperty(INDUSTRY)
+					);
+		}
+	};
+
 	public AllScriptsDbObject(String nseId, String stockName, String isin, String bseId, String fairValue, String industry) {
 		this.primaryKey=nseId;
 		this.nseId= nseId;
@@ -45,22 +64,38 @@ public class AllScriptsDbObject extends DbObject{
 
 	public String retrieveBseIdForNse(String nseId)
 	{
-		Filter filter = new FilterPredicate(NSE_ID, FilterOperator.EQUAL, nseId);
-		Query query = new Query(this.getTableName()).setFilter(filter);
+		List<AllScriptsDbObject> dbObjects = retrieveAllRecords();
 		
-		DbOperations dbOperations = DbOperations.getInstance();
-		Entity entity = dbOperations.retrieveEntity(query);
-		return this.convertToDbObject(entity).getBseId();
+		for(AllScriptsDbObject dbObject : dbObjects)
+		{
+			if(dbObject.getNseId().equals(nseId))
+			{
+				return dbObject.getBseId();
+			}
+		}
+		return null;
 	}
 
 	public String retrieveNseIdForBse(String bseId)
 	{
-//		Filter filter = new FilterPredicate(BSE_ID, FilterOperator.EQUAL, bseId);
-		Query query = new Query(this.getTableName());
+		List<AllScriptsDbObject> dbObjects = retrieveAllRecords();
 		
+		for(AllScriptsDbObject dbObject : dbObjects)
+		{
+			if(dbObject.getBseId().equals(bseId))
+			{
+				return dbObject.getNseId();
+			}
+		}
+		return null;
+	}
+
+	private List<AllScriptsDbObject> retrieveAllRecords() {
+		Query query = new Query(this.getTableName());
 		DbOperations dbOperations = DbOperations.getInstance();
 		List<Entity> entities = FastList.newList(dbOperations.retrieveEntities(query, false));
-		return null;
+		List<AllScriptsDbObject> dbObjects = ((FastList)entities).collect(DB_OBJECT_SELECTOR);
+		return dbObjects;
 	}
 	
 	
