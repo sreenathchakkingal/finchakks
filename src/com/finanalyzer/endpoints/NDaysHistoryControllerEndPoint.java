3d845fca82fd226710c88881193804e2c3b36109
sreenathchakkingal <sreenathchakkingal04@gmail.com>
2017-07-16 13:06:35 +0530

    organize imports

--- src/com/finanalyzer/endpoints/NDaysHistoryControllerEndPoint.java
+++ src/com/finanalyzer/endpoints/NDaysHistoryControllerEndPoint.java
@@ -16,53 +16,64 @@ import com.google.api.server.spi.config.Named;
 
 @Api(name = "nDaysHistoryControllerEndPoint", version = "v1")
 public class NDaysHistoryControllerEndPoint {
-	
+
 	@ApiMethod(name = "refreshNDaysHistoryStocks")
-	public Collection<NDaysHistoryDbObject> refreshNDaysHistoryStocks(@Named("numOfDays") String numOfDays, @Named("simpleMovingAverage") String simpleMovingAverage)
-	{
-		List<Stock> stocks = new QuandlNDaysPricesProcessor(numOfDays, simpleMovingAverage).execute();
-		
+	public Collection<NDaysHistoryDbObject> refreshNDaysHistoryStocks(
+			@Named("numOfDays") String numOfDays,
+			@Named("simpleMovingAverage") String simpleMovingAverage) {
+		List<Stock> stocks = new QuandlNDaysPricesProcessor(numOfDays,
+				simpleMovingAverage).execute();
+
 		List<NDaysHistoryDbObject> ndaysHistoryDbObjects = stampDetailsFromUnrealized(stocks);
-		
+
 		return persistResult(ndaysHistoryDbObjects);
 	}
 
-	protected List<NDaysHistoryDbObject> stampDetailsFromUnrealized(List<Stock> stocks) 
-	{
-		List<NDaysHistoryDbObject> ndaysHistoryDbObjects = ConverterUtil.stockToNdaysHistoryDbObject(stocks);
+	protected List<NDaysHistoryDbObject> stampDetailsFromUnrealized(
+			List<Stock> stocks) {
+		List<NDaysHistoryDbObject> ndaysHistoryDbObjects = ConverterUtil
+				.stockToNdaysHistoryDbObject(stocks);
 
-		JdoDbOperations<UnrealizedSummaryDbObject> unrealizedSummaryDbOperations = new JdoDbOperations<>(UnrealizedSummaryDbObject.class);
-		JdoDbOperations<UnrealizedDetailDbObject> unrealizedDetailDbOperations = new JdoDbOperations<>(UnrealizedDetailDbObject.class);
+		JdoDbOperations<UnrealizedSummaryDbObject> unrealizedSummaryDbOperations = new JdoDbOperations<>(
+				UnrealizedSummaryDbObject.class);
+		JdoDbOperations<UnrealizedDetailDbObject> unrealizedDetailDbOperations = new JdoDbOperations<>(
+				UnrealizedDetailDbObject.class);
 
-		final List<UnrealizedSummaryDbObject> unrealizedSummaryDbObjects = unrealizedSummaryDbOperations.getEntries("stockName");
-		final List<UnrealizedDetailDbObject> unrealizedDetailDbObjects = unrealizedDetailDbOperations.getEntries("buyDate asc");
+		final List<UnrealizedSummaryDbObject> unrealizedSummaryDbObjects = unrealizedSummaryDbOperations
+				.getEntries("stockName");
+		final List<UnrealizedDetailDbObject> unrealizedDetailDbObjects = unrealizedDetailDbOperations
+				.getEntries("buyDate asc");
 
-		for (NDaysHistoryDbObject ndaysHistoryDbObject :  ndaysHistoryDbObjects)
-		{
-			for (UnrealizedSummaryDbObject unrealizedSummaryDbObject :  unrealizedSummaryDbObjects)
-			{
-				if(unrealizedSummaryDbObject.getStockName().equals(ndaysHistoryDbObject.getMoneyControlName()))
-				{
-					ndaysHistoryDbObject.setReturnTillDate(unrealizedSummaryDbObject.getReturnTillDate());
-					ndaysHistoryDbObject.setImpactOnAverageReturn(unrealizedSummaryDbObject.getImpactOnAverageReturn());
+		for (NDaysHistoryDbObject ndaysHistoryDbObject : ndaysHistoryDbObjects) {
+			for (UnrealizedSummaryDbObject unrealizedSummaryDbObject : unrealizedSummaryDbObjects) {
+				if (unrealizedSummaryDbObject.getStockName().equals(
+						ndaysHistoryDbObject.getMoneyControlName())) {
+					ndaysHistoryDbObject
+							.setReturnTillDate(unrealizedSummaryDbObject
+									.getReturnTillDate());
+					ndaysHistoryDbObject
+							.setImpactOnAverageReturn(unrealizedSummaryDbObject
+									.getImpactOnAverageReturn());
 				}
 			}
-			
-			for (UnrealizedDetailDbObject unrealizedDetailDbObject :  unrealizedDetailDbObjects)
-			{
-				if(unrealizedDetailDbObject.getStockName().equals(ndaysHistoryDbObject.getMoneyControlName()))
-				{
-					ndaysHistoryDbObject.setDuration(unrealizedDetailDbObject.getDuration());
-					ndaysHistoryDbObject.setBuyPrice(unrealizedDetailDbObject.getBuyPrice());
+
+			for (UnrealizedDetailDbObject unrealizedDetailDbObject : unrealizedDetailDbObjects) {
+				if (unrealizedDetailDbObject.getStockName().equals(
+						ndaysHistoryDbObject.getMoneyControlName())) {
+					ndaysHistoryDbObject.setDuration(unrealizedDetailDbObject
+							.getDuration());
+					ndaysHistoryDbObject.setBuyPrice(unrealizedDetailDbObject
+							.getBuyPrice());
 				}
 			}
 		}
 		return ndaysHistoryDbObjects;
 	}
-	
-	private Collection<NDaysHistoryDbObject> persistResult(List<NDaysHistoryDbObject> ndaysHistoryDbObjects) 
-	{
-		JdoDbOperations<NDaysHistoryDbObject>  dbOperations = new JdoDbOperations<>(NDaysHistoryDbObject.class);
+
+	private Collection<NDaysHistoryDbObject> persistResult(
+			List<NDaysHistoryDbObject> ndaysHistoryDbObjects) {
+		JdoDbOperations<NDaysHistoryDbObject> dbOperations = new JdoDbOperations<>(
+				NDaysHistoryDbObject.class);
 		return dbOperations.deleteAndInsertEntries(ndaysHistoryDbObjects);
-	}  
+	}
 }
